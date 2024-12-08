52a389401b9d20490c282dd198c1aaf60825560a
Nidhi Maurya <mauryanidhi1610@gmail.com>
2024-12-08 14:44:42 +0530

    eecploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ const express=require('express');
 const app=express();
 const port=7777;
 
- app.use("/hello",(req,res)=>{
-res.send("hello");
-});
+ 
 
+// App.get only handle GET call to /user
+
+app.get("/user",(req,res)=>{
+  res.send({firstname:"NIDHI",lastname:"MAURYA"});
+})
+
+app.post("/user",(req,res)=>{
+  res.send("data save successfully save to database")
+})
+
+app.delete("/user",(req,res)=>{
+  res.send("deleted successfully");
+})
 
 app.use("/home",(req,res)=>{
   res.send("this is my home page");
 });
+// app.use is match all the HTTP method API calls to /test
 
-app.listen(3000,()=>{
+app.listen(7777,()=>{
   console.log(`Server is running on port ${port}`);
   
 })
