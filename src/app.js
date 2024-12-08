0c173c76414d3d9a577240264544e1855c2708df
Nidhi Maurya <mauryanidhi1610@gmail.com>
2024-12-08 15:33:31 +0530

    route explore

--- src/app.js
+++ src/app.js
@@ -6,16 +6,25 @@ const port=7777;
 
 // App.get only handle GET call to /user
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-  console.log(req.params);
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+//   console.log(req.params);
   
-  res.send({firstname:"NIDHI",lastname:"MAURYA"});
-})
+//   res.send({firstname:"NIDHI",lastname:"MAURYA"});
+// })
 
 
 
-app.use("/home",(req,res)=>{
-  res.send("this is my home page");
+app.use("/user",(req,res,next)=>{
+  // route handler
+  console.log("handling route user 1!!");
+  // res.send("route handler 1");
+  next();
+},
+(req,res)=>{
+  //route handler 2
+  console.log("handling route user 2!!");
+  res.send("handler 2");
+  
 });
 // app.use is match all the HTTP method API calls to /test
 
