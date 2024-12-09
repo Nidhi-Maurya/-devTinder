a354e3e66b14153e48de88484afee8f5369d3124
Nidhi Maurya <mauryanidhi1610@gmail.com>
2024-12-09 20:32:10 +0530

    delete update API

--- src/app.js
+++ src/app.js
@@ -29,7 +29,24 @@ app.get("/user", async (req, res) => {
   }
 });
 
+app.get ("/user",async (req,res)=>{
+  const userId=req.body.userId;
+  try {
+    const users=await User.findById(userId);
+    res.send("user find");
+  } catch (error) {
+
+     res.status(400).send("Something went wrong!!");
+  }
+})
+
+
+
+
 // feed API -get /feed -all the users from the database
+ 
+// DELETE API
+
 app.get("/feed",async (req, res) => {
   try {
     const users=  await User.find({});
@@ -38,6 +55,34 @@ app.get("/feed",async (req, res) => {
     res.status(400).send("Something went wrong!!");
   }
 });
+
+//  DELETE API
+
+app.delete("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  try {
+    const user=await User.findByIdAndDelete(userId);
+
+     res.send("user deleted successfully!!")
+  } catch (error) {
+    res.status(400).send("Something went wrong!!");
+  }
+})
+
+// update data of the user
+
+app.patch("/user",async(req,res)=>{
+  const userId= req.body.userId;
+  const data=req.body;
+  try {
+    await User.findByIdAndUpdate({_id:userId},data);
+    res.send("user updated successfully");
+  } catch (error) {
+     res.status(400).send("Something went wrong");
+  }
+})
+
+
 
 connectDB().then(() => {
   try {
